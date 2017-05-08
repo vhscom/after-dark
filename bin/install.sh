c0390cd3da681d34320e1e24f446c85b98dcc2fe
vhs <vhsdev@tutanota.com>
2017-05-08 20:36:47 +0800

    fix(install): improve install experience
    fix a race condition in elinks opening and provide next steps at end of script

--- bin/install.sh
+++ bin/install.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+SITE_SOURCE_PATH=$(pwd)
 HUGO_CONFIG_PATH="./config.toml"
+HUGO_SERVE_PID=""
+ELINKS_PID=""
 
 # Create new site
 if [[ $1 != "" ]]; then
+  SITE_SOURCE_PATH="${SITE_SOURCE_PATH}/$1"
   hugo new site $1 && cd $_
 else
+  SITE_SOURCE_PATH="${SITE_SOURCE_PATH}/flying-toasters"
   hugo new site flying-toasters && cd $_
 fi
-echo "\nInstalling After Dark..."
+
+echo "\nInstalling After Dark ..."
 
 # Clone repo
 (cd themes; git clone -q --depth 1 https://codeberg.org/vhs/after-dark || { echo "cloning failed :/"; exit 1; })
@@ -43,13 +49,25 @@ footnoteReturnLinkContents = "↩" # Provides a nicer footnote return link
   theme_variant = "" # Optional, for use to overriding default theme
 EOF
 
-# Create new post using instructional post archetype
+echo "\nCreating an example post to get you started ..."
 hugo new post/starry-night.md
+
+echo "Serving your After Dark site ..."
 
 # Serve site backgrounded over Docker-friendly loopback
 hugo serve --buildDrafts --port 1337 --bind "0.0.0.0" &
+
+# Wait a second for Hugo server to fire up
+sleep 1
 
 # Pop the site in terminal browser, if installed
 if [[ "elinks" != "" ]]; then
   elinks http://0.0.0.0:1337/
 fi
+
+echo "Installation complete! Your new After Dark site is created in $SITE_SOURCE_PATH."
+echo "\nSite is currently running at http://0.0.0.0:1337/"
+echo "To stop it run \"kill \$(lsof -nt -i4TCP:1337)\"."
+echo "\nThank you for choosing After Dark."
+
+exit 0
